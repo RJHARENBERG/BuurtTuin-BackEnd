4d84ed28a211d3b943c1c335d4b138e3cab8cf89
RJHARENBERG <reinardharenberg@gmail.com>
2022-05-16 17:41:24 +0200

    opzet van deelopdracht 2

---
+++ src/main/java/com/example/buurttuin/BuurtTuinApplication.java
@@ -0,0 +1,13 @@
+package com.example.buurttuin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuurtTuinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuurtTuinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/buurttuin/BuurtTuinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buurttuin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuurtTuinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
